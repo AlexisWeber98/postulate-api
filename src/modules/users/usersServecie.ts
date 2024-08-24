0f56563d0b8677bebca4ad44d66368f2c53dd829
Ale David Weber <Alexis.Weber@mailfence.com>
2024-08-23 23:25:20 -0300

    add user services, create and login

--- src/modules/users/usersServecie.ts
+++ src/modules/users/usersServecie.ts
@@ -15,3 +15,17 @@ export const createUser = async (name: string, lastName: string, userName: strin
         throw error;
     };
 };
+
+export const login = async (email: string, password: string) => {
+    try { 
+        const user = await User.findOne({ where: { email } });
+        
+        if (!user) throw new Error("User not Found");
+        
+        if (user.password !== password) throw new Error("Passord not match");
+        
+        return user;
+    } catch (error) {
+        throw error;
+    };
+}
