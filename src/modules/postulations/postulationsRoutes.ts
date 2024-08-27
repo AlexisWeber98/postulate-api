2b27b736c7ea54b6266d6c605f8e6c7c53205e57
Ale David Weber <Alexis.Weber@mailfence.com>
2024-08-26 21:48:02 -0300

    delÃete usless files

--- src/modules/postulations/postulationsRoutes.ts
+++
@@ -1,6 +0,0 @@
-import {postPostulationController, getAllPostulationsController} from "./postulationController";
-
-const router = require("express").Router();
-
-router.post("/postulation", postPostulationController);
-router.get("/postulations", getAllPostulationsController);
--- src/modules/users/userRoutes.ts
+++
@@ -1,6 +0,0 @@
-import { createUserController, loginController } from "./usersControllers";
-
-const route = require('express').Route();
-
-route.post("/user", createUserController);
-route.get("/login", loginController);
