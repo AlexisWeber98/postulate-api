f7327950b7a423a3c47afb2f1f8710d27ea02793
Ale David Weber <Alexis.Weber@mailfence.com>
2024-08-26 21:46:25 -0300

    add function postulations imports

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,9 +1,13 @@
 import { createUserController, loginController } from "../modules/users/usersControllers";
 import { postPostulationController, getAllPostulationsController } from "../modules/postulations/postulationController";
+import { getAllPostulations } from "../modules/postulations/postulationService";
 
 const route = require('express').Router();
 
 route.post("/user", createUserController);
+route.post("/postulation", postPostulationController)
+
+route.get("/postulations", getAllPostulations)
 route.get("/login", loginController);   
 
 export = route;
