088886618b82b8aff913951dce8f1b33bead8132
Ale David Weber <Alexis.Weber@mailfence.com>
2024-08-26 21:47:02 -0300

    add user controllers

--- src/modules/users/usersControllers.ts
+++ src/modules/users/usersControllers.ts
@@ -4,32 +4,32 @@ import { ReqUserBody } from "./interface";
 import { createUser, login } from './usersServecie';
 
 
-export const createUserController = async (req: Request, res: Response) => {
-  
-    const { name, lastName, userName, email, password } :ReqUserBody = req.body;
+export const createUserController = async (req:Request, res:Response) => {
+   
+        const { name, lastName, userName, email, password }: ReqUserBody = req.body;
     
-    const errors = validationPostUser(name, lastName, userName, email, password);
+        const errors = validationPostUser(name, lastName, userName, email, password);
     
-    try { 
-    if (errors) return res.status(400).json(errors);
+        try {
+            if (errors) return res.status(400).json(errors);
     
-    const data = await createUser(name, lastName, userName, email, password);
+            const data = await createUser(name, lastName, userName, email, password);
     
-    const response = {
-      result: "Ok",
-      data  
-    };
+            const response = {
+                result: "Ok",
+                data
+            };
     
-    res.status(200).json(response)
+            res.status(200).json(response)
         
-    } catch (error){
-        const response = {
-            result: "Error",
-            error,
-        };
-        res.status(500).json(response);
+        } catch (error) {
+            const response = {
+                result: "Error",
+                error,
+            };
+            res.status(500).json(response);
+        }
     }
-}
 
 export const loginController = async (req:Request, res:Response) => { 
     const { userName, password } = req.body; 
