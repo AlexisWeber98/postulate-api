1eb456d2907303abaaf873605ae06fd5346d593d
Ale David Weber <Alexis.Weber@mailfence.com>
2024-08-28 23:38:24 -0300

    add userId to model

--- src/models/PostulationsModel.ts
+++ src/models/PostulationsModel.ts
@@ -10,57 +10,61 @@ enum StatusPostulation {
 }
 
 export function definePostulationsModel(sequelize: Sequelize) {
-  const Postulation = sequelize.define("Postulations", {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-    },
-    date: {
-      type: DataTypes.DATEONLY,
-      allowNull: false,
-    },
-    company: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    position: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    trough: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
-    status: {
-      type: DataTypes.STRING,
-      defaultValue: StatusPostulation.Pending,
-      validate: {
-        isIn: [Object.values(StatusPostulation)], //asegura que el valor del enum sea válido
-      },
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: true,
-    },
+    const Postulation = sequelize.define("Postulations", {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+        },
+        date: {
+            type: DataTypes.DATEONLY,
+            allowNull: false,
+        },
+        company: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        position: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        trough: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        status: {
+            type: DataTypes.STRING,
+            defaultValue: StatusPostulation.Pending,
+            validate: {
+                isIn: [Object.values(StatusPostulation)], //asegura que el valor del enum sea válido
+            },
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
 
-    sendEmail: {
-      type: DataTypes.BOOLEAN,
-      allowNull: true,
-    },
-    sendCv: {
-      type: DataTypes.BOOLEAN,
-      allowNull: true,
-    },
-    recruiterContact: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    userId: {
-      type: DataTypes.UUID,
-      allowNull: false,
-    },
-  });
+        sendEmail: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        },
+        sendCv: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+        },
+        recruiterContact: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        userId: {
+            type: DataTypes.STRING,
+            references: {
+                model: 'Users', // Nombre de la tabla de usuarios
+                key: 'id'
+            },
+            allowNull: false
+        },
+    })
 
   return Postulation;
 }
