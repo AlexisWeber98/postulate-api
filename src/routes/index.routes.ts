0f275e42809a63b63f8c697dded62a082a3b7c18
Ale David Weber <Alexis.Weber@mailfence.com>
2024-08-30 15:16:32 -0300

    add get postulation  by id route

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,11 +1,13 @@
 import { createUserController, loginController } from "../modules/users/usersControllers";
-import { postPostulationController, getAllPostulationsController } from "../modules/postulations/postulationController";
+import { postPostulationController, getAllPostulationsController, getAllPostulationByIdController } from "../modules/postulations/postulationController";
+
 const route = require('express').Router();
 
 // postulations
 
 route.post("/postulation", postPostulationController)
 route.get("/postulations", getAllPostulationsController)
+route.get("/postulation/:id", getAllPostulationByIdController)
 
 
 // Users
