76150dfd532904d00feccf8ec7d9df28d1550f3d
Alexis Weber <Alexis.weber@mailfence.com>
2025-01-14 22:11:07 -0300

    update routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,22 +1,32 @@
-import { createUserController, loginController, userUpdateController, deleteUserController } from "../modules/users/usersControllers";
-import { postPostulationController, getAllPostulationsController, getPostulationByIdController, updatePostulationController, deletePostulationController } from "../modules/postulations/postulationController";
+import {
+  createUserController,
+  loginController,
+  userUpdateController,
+  deleteUserController,
+} from "../modules/users/usersControllers";
+import {
+  postPostulationController,
+  getAllPostulationsController,
+  getPostulationByIdController,
+  updatePostulationController,
+  deletePostulationController,
+} from "../modules/postulations/postulationController";
 
-const route = require('express').Router();
+const route = require("express").Router();
 
 // { ------------- Postulations --------------- } \\
 
-route.post("/postulation", postPostulationController)
-route.get("/postulations", getAllPostulationsController)
-route.get("/postulation/:id", getPostulationByIdController)
-route.patch("/postulation", updatePostulationController)
-route.delete('/postulation/:id', deletePostulationController)
-
+route.post("/postulation", postPostulationController);
+route.get("/postulations/:id", getAllPostulationsController);
+route.get("/postulation/:id", getPostulationByIdController);
+route.patch("/postulation", updatePostulationController);
+route.delete("/postulation/:id", deletePostulationController);
 
 // { ------------- Users --------------- } \\
 
 route.post("/user", createUserController);
-route.get("/login", loginController);   
-route.patch('/user/:id', userUpdateController)
-route.delete('/usrer/:id', deleteUserController)
+route.get("/login", loginController);
+route.patch("/user/:id", userUpdateController);
+route.delete("/usrer/:id", deleteUserController);
 
 export = route;
