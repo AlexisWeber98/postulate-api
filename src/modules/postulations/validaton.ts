f00c7c1b2487b439556a9e3eee005d42a6c24d64
Ale Weber <alexis.weber@mailfence.com>
2025-03-17 20:08:05 -0300

    refactor and fix validations

--- src/modules/postulations/validaton.ts
+++ src/modules/postulations/validaton.ts
@@ -1,15 +1,14 @@
 export function validationPostPostulation(
-  date: string,
+  aplicationDate: string,
   position: string,
   company: string,
-  trough: string,
-  userId:string,
+
+  userId: string,
 ) {
-if (!userId) return { message: "userId is required" };
-  if (!date) return { message: "date is required" };
+  if (!userId) return { message: "userId is required" };
+  if (!aplicationDate) return { message: "date is required" };
   if (!position) return { message: "position is required" };
   if (!company) return { message: "company is required" };
-  if (!trough) return { message: "trough is required" };
   else {
     return null;
   }
