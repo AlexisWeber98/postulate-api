5c5cfd6c4d983e40b05c28f15aab1dd2516af83d
Ale Weber <alexis.weber@mailfence.com>
2025-04-04 20:18:21 -0300

    add swagger configuration

---
+++ src/config/swagger.ts
@@ -0,0 +1,27 @@
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+import { Express } from "express";
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Postulate-API",
+      version: "1.0.0",
+      description: "API documentation for Postulate",
+    },
+    servers: [
+      {
+        url: "http://localhost:3001",
+        description: "Development local server",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts", "./src/models/*.ts"],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+export const setupSwagger = (app: Express): void => {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
