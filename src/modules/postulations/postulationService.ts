71f74b2b1bef44df57d72898c1bd2ea3299fa285
Ale Weber <alexis.weber@mailfence.com>
2025-04-04 20:14:32 -0300

    RF: delete usless imports

--- src/modules/postulations/postulationService.ts
+++ src/modules/postulations/postulationService.ts
@@ -35,9 +35,6 @@ export const postPostulationService = async (body: ReqPostBody) => {
       sendEmail,
       recruiterContact,
     });
-    console.log(" { ----------- BODY: ------------} ", body);
-
-    console.log(" { ----------- DATA: ------------} ", data);
 
     return data ? data : "withouth data";
   } catch (error) {
