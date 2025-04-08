f1b65d9dbe4bd88fdddf85c438ce254e3281710f
Ale Weber <alexis.weber@mailfence.com>
2025-04-07 21:10:28 -0300

    RF: modularization userRoutes

---
+++ src/modules/users/userRoutes.ts
@@ -0,0 +1,63 @@
+import express from 'express';
+import {
+  createUserController,
+  loginController,
+  userUpdateController,
+  deleteUserController,
+} from "./usersControllers.js";
+
+const userRouter = express.Router();
+
+/**
+ * @swagger
+ * tags:
+ *   - name: Users
+ *     description: Operaciones relacionadas con usuarios
+ */
+
+/**
+ * @swagger
+ * /user:
+ *   post:
+ *     summary: Registrar un nuevo usuario
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       201:
+ *         description: Usuario creado exitosamente
+ */
+userRouter.post("/", createUserController);
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Iniciar sesión
+ *     tags: [Users]
+ */
+userRouter.post("/login", loginController);
+
+/**
+ * @swagger
+ * /user/{id}:
+ *   patch:
+ *     summary: Actualizar información de usuario
+ *     tags: [Users]
+ */
+userRouter.patch("/:id", userUpdateController);
+
+/**
+ * @swagger
+ * /user/{id}:
+ *   delete:
+ *     summary: Eliminar usuario
+ *     tags: [Users]
+ */
+userRouter.delete("/:id", deleteUserController);
+
+export { userRouter };
