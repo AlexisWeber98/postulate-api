47d770d20cf9e53e8bbcebff701107dd9c3c2a42
Ale Weber <alexis.weber@mailfence.com>
2025-04-07 21:08:01 -0300

    FT: add error handler catchAsync

---
+++ src/utils/catchAsync.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+export const catchAsync = (
+  controller: (
+    req: Request,
+    res: Response,
+    next?: NextFunction,
+  ) => Promise<any>,
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    controller(req, res).catch((error) => next(error));
+  };
+};
