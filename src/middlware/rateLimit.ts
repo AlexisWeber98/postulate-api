2296256a451cedc71a9cee0865294a923de957d8
Ale Weber <alexis.weber@mailfence.com>
2025-05-19 11:32:41 -0300

    feat: add rate limit middleware

--- src/middlware/rateLimit.ts
+++ src/middlware/rateLimit.ts
@@ -1,23 +1,24 @@
-import rateLimit from 'express-rate-limit';
-import { Logger } from '../utils/logger.js';
+import { NextFunction, Request, Response } from "express";
+import rateLimit from "express-rate-limit";
+import { Logger } from "../utils/logger.js";
 
 export const generalLimiter = rateLimit({
-    windowMs: 15*60*1000,
-    max:100
-})
-
+  windowMs: 15 * 60 * 1000,
+  max: 6,
+});
 
 export const authLimiter = rateLimit({
-    windowMs: 59*60*1000,
-    max: 5,
-    handler: (req, res) => {
-        Logger.warn('Rate limit exceeded', {
-            ip: req.ip,
-            path: req.path
-        });
-        res.status(428).json({
-            status: 'error',
-            message: 'Too many login attempts. Please try again in 5 minutes.'
-        });
-    }
-})
+  windowMs: 59 * 60 * 1000,
+  max: 200,
+  handler: (req: Request, res: Response) => {
+    Logger.warn("Rate limit exceeded", {
+      ip: req.ip,
+      path: req.path,
+    });
+    res.status(428).json({
+      status: "error",
+      message: "Too many login attempts. Please try again in 5 minutes.",
+    });
+  },
+});
+
