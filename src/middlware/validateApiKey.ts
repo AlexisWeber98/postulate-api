a0d59ad060bcee8401593a544654db2bce305804
Ale Weber <alexis.weber@mailfence.com>
2025-05-18 21:50:15 -0300

    fix: update api key validation

--- src/middlware/validateApiKey.ts
+++ src/middlware/validateApiKey.ts
@@ -1,20 +1,15 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from "express"
 
-export const validateApiKey = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const apiKey = req.headers["x-api-key"];
+export const validateApiKey = ( req:Request, res:Response, next: NextFunction) => {
+    const apiKey = req.headers['x-api-key'];
 
-  if (!apiKey) {
-    return res.status(401).json({ message: "API Key not found" });
-  }
+    if(!apiKey) {
+        return res.status(401).json({message: "API Key not found"})
+    };
 
-  if (apiKey !== process.env.API_KEY) {
-    return res.status(4001).json({ message: "Invalid API Key" });
-  }
+    if(apiKey !== process.env.API_KEY) {
+        return res.status(4001).json({message: "Invalid API Key"})
+    }
 
-  next();
-};
-
+    next()
+}
