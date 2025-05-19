3875d7a80a4582bafdb3b818806d1820f8a9ac61
Ale Weber <alexis.weber@mailfence.com>
2025-05-18 21:25:51 -0300

    feat: update user service

--- src/modules/users/usersService.ts
+++ src/modules/users/usersService.ts
@@ -1,12 +1,12 @@
 import db from "../../db.js";
 import { Model } from "sequelize";
-import { UserModelInterface } from "../../models/modelTypes.js";
+import { UserModelInterface } from "./interface.js";
 import { DatabaseError, AuthenticationError } from "../../utils/errors.js";
 import { hashPassword, verifyPassword } from "../../utils/hashPassword.js";
-
+import { generateToken } from "../../utils/jwt.js";
 import { Logger } from "../../utils/logger.js";
 
-const { User } = db.models;
+const { User: SequelizeUser } = db.models;
 
 export const createUser = async (
   name: string,
@@ -18,8 +18,7 @@ export const createUser = async (
   try {
     const passwordHashed = await hashPassword(password.trim());
 
-    console.log("passwordHashed", passwordHashed);
-    const data = await User.create({
+    const data = await SequelizeUser.create({
       name,
       lastName: lastName.trim(),
       userName: userName.trim(),
@@ -41,30 +40,26 @@ export const createUser = async (
 };
 
 export const login = async (email: string, password: string) => {
-  try {
-    const user = (await User.findOne({ where: { email } })) as Model<
-      UserModelInterface,
-      any
-    >;
-
-    if (!user) throw new AuthenticationError("User not Found");
-
-    const storedPassword = user.get("password") as string;
-
-    const isPasswordValid = await verifyPassword(storedPassword, password);
-    if (!isPasswordValid) throw new AuthenticationError("Password not match");
-
-    Logger.info("User logged in", { userId: user.get("id") });
-
-    return user;
-  } catch (error) {
-    if (error instanceof AuthenticationError) throw error;
+  const user = (await SequelizeUser.findOne({
+    where: { email },
+  })) as Model<UserModelInterface>;
+  if (!user) {
+    throw new AuthenticationError("Invalid credentials");
   }
+
+  const storedPassword = user.get("password") as string;
+  const isPasswordValid = await verifyPassword(storedPassword, password);
+  if (!isPasswordValid) {
+    throw new AuthenticationError("Invalid credentials");
+  }
+
+  const token = generateToken(user.get());
+  return token;
 };
 
 export const userUpadeService = async (userId: string, data: object) => {
   try {
-    const user = await User.findByPk(userId);
+    const user = await SequelizeUser.findByPk(userId);
 
     if (!user) throw new AuthenticationError("User not found");
 
