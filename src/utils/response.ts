7675adc469b0a8e302f2bf4c87f79a22fba137fa
Ale Weber <alexis.weber@mailfence.com>
2025-05-18 21:26:14 -0300

    RF: refactor upate response util

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,4 +1,7 @@
-export default function serverResponse(statusResponse: string, result: object) {
+export default function serverResponse(
+  statusResponse: string,
+  result: object | string,
+) {
   return {
     statusResponse,
     result,
