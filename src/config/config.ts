503972e45a5461ed66c23006ce530276d9649534
Ale Weber <alexis.weber@mailfence.com>
2025-05-26 16:11:25 -0300

    feat: update config environments

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,3 +8,7 @@ export const dbHost = process.env.DB_HOST;
 export const dbPassword = process.env.DB_PASSWORD;
 export const dbPort = process.env.DB_PORT;
 export const dbUser = process.env.DB_USER;
+export const frontendUrl = process.env.FRONTEND_URL;
+export const frontendUrlWww = process.env.FRONTEND_URL_WWW;
+export const frontendUrlDevelop1 = process.env.FRONTEND_URL_DEVELOP1;
+export const frontendUrlDevelop2 = process.env.FRONTEND_URL_DEVELOP2;
