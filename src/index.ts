652b81c6e771566ddfa60f2745bd7be67cb8c158
Ale Weber <alexis.weber@mailfence.com>
2025-05-27 10:57:58 -0300

    RF: clean db

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ db.sequelize
   .authenticate()
   .then(() => {
     console.log("Conexión establecida con éxito.");
-    return db.sequelize.sync({ force: false });
+    return db.sequelize.sync({ force: true });
   })
   .then(() => {
     setupSwagger(app);
