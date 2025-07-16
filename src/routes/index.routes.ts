5fc10b6d60e3a99dfeff408b50f16cee791d107c
Ale Weber <alexis.weber@mailfence.com>
2025-07-15 21:52:50 -0300

    chore: update main router to include new modules and routes in index.routes.ts

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { userRouter } from "../modules/users/userRoutes.js";
 import { postulationRouter } from "../modules/postulations/postulationRoutes.js";
+import { iaRouter } from "../modules/IAModule/iaRoutes.js";
+import { emailRouter } from "../modules/emailModule/emailRoutes.js";
 
 const router = express.Router();
 
@@ -9,5 +11,10 @@ router.use("/users", userRouter);
 
 // Rutas de postulaciones
 router.use("/postulations", postulationRouter);
+
+router.use("/email", emailRouter);
+
+//Rutas para IA
+router.use("/ia", iaRouter);
 
 export default router;
